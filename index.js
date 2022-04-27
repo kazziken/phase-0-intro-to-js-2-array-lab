7336f7f520057878eb803385e5703a503b49e51c
Kenneth Mei <meikenneth321@gmail.com>
2022-04-27 10:11:45 -0400

    commit tree b7b585a2b1aaca98b0b81b76c3726ecfdec7159a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+}
+
+function appendCat(){
+    const newLastCats = [...cats, "Broom"];
+    return newLastCats;
+}
+
+function prependCat(){
+    const newFirstCat = ["Arnold", ...cats];
+    return newFirstCat;
+}
+
+function removeFirstCat(){
+    const byeFirstCat = cats.slice(1);
+    return byeFirstCat;
+}
+
+function removeLastCat(){
+    const byeLastCat = cats.slice(0,-1);
+    return byeLastCat;
+}
